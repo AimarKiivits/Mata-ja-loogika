693b44f40bc144ac5b251bef76df913420704482
Knack <Aimarkiivits1@gmail.com>
2022-10-19 10:03:24 +0300

    Exercise: Mathematicla expressions

---
+++ Math_expressions.py
@@ -0,0 +1,43 @@
+import math
+
+"""Practice different math expressions and calculations."""
+
+
+
+#declare num_a and num_b
+num_a = 4
+num_b = 2
+radius = 6
+side_lenght = 5
+
+#num_a and num_b sum and difference
+sum = num_a + num_b
+print(sum)
+difference = num_a - num_b
+
+#num_a and num_b float division
+division_float = num_a / num_b
+
+#num_a and num_b int division
+division_int = num_a // num_b
+
+#num_a times num_b
+multiply_numbers = num_a * num_b
+
+#num_a to the power of num_b
+power = num_a ** num_b
+
+#remainder of num_a divided by num_b
+remainder = num_a % num_b
+
+#average of num_a and num_b
+average = (num_a + num_b) / 2
+
+#area of circle
+circle_area = round(math.pi * (radius ** 2), 2)
+
+#
+
+
+
+
