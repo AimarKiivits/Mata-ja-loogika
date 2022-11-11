788eb6712a5752f5fd31789d0c692603261d34ea
Knack <Aimarkiivits1@gmail.com>
2022-11-11 09:11:59 +0200

    Exercise: String

---
+++ String.py
@@ -0,0 +1,15 @@
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + " " + last_name
+#print(full_name)
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+#print(self_desription_sentence)
+cake = "vahukoormarjadtäidispõhi"
+print("vahukoor\nmarjad\ntäidis\npõhi")
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+#print(backwards)
+every_other = original_string[::2]
+#print(every_other)
+first_word_reversed = original_string[10::-1]
+#print(first_word_reversed)
